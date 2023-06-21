b14fb8e0ef9def79367045c135508335dba87082
wilsonhuang01 <wilsonpwhuang@gmail.com>
2023-06-21 16:08:13 -0700

    summary page update

--- src/main/java/com/example/momuney2/controllers/SummaryControllerV2.java
+++ src/main/java/com/example/momuney2/controllers/SummaryControllerV2.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +14,32 @@ import java.io.IOException;
 public class SummaryControllerV2 {
 
     @FXML Button summaryButton;
+
+    @FXML ChoiceBox<String> summaryOptions;
+    @FXML ChoiceBox<String> chartOptions;
+    @FXML DatePicker dateFrom;
+    @FXML DatePicker dateTo;
+
+    /*
+        show summary for
+                        spending by category
+                            barchart & piechart (all-time or within time range)
+                        spending by date
+                            linechart & stacked barhcart (each category represent a line)
+                            barchart (combined)
+                        spending by vendor (name or location)
+                            barchart & piechart (all time or within time range)
+     */
+
+    @FXML
+    private void initialize() {
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        summaryOptions.getItems().addAll("Vendor (Name)", "Vendor (Location)", "Category", "Date");
+        summaryOptions.setValue("Category");
+    }
 
     @FXML
     private void changeToNewTransactionPage() {
